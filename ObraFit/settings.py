28fb5b8cc617b75e9741b485540b83f9888ab9e8
Eternityexodial <nicolasherreramontes625@gmail.com>
2025-11-10 12:17:31 -0300

    develop: cambios de letra y texto mas activacion de funcionalidad SMTP para contraseña

--- ObraFit/settings.py
+++ ObraFit/settings.py
@@ -178,3 +178,17 @@ MESSAGE_TAGS = {
     messages.ERROR: 'error',
 }
 
+# Configuración de Email
+# OPCIÓN 1: Para desarrollo - muestra emails en la consola (recomendado para probar)
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# OPCIÓN 2: Para producción - usando Gmail SMTP
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'ObraFit4@gmail.com'
+EMAIL_HOST_PASSWORD = 'gpyn cecm ubtk fgnx'
+DEFAULT_FROM_EMAIL = 'ObraFit <ObraFit04@gmail.com>'
+SERVER_EMAIL = 'ObraFit04@gmail.com'
+
